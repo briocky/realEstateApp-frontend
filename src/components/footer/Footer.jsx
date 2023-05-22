9da38daa2f73fe1b83b21e2ded238d50740510aa
diminuen <dawid.dobosz.stud@pw.edu.pl>
2023-05-22 20:23:26 +0200

    footer added

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,18 +1,34 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Link, Typography } from "@mui/material";
 import logo from "../../assets/logo_v8_no_bg_colors.png";
 
 export default function Footer() {
   return (
-    <Box sx={{ backgroundColor: "grey", display: "flex" }}>
-      <Box sx={{ display: "flex" }}>
-        <img src={logo} />
-        <Typography variant="h6">nowaposiadlosc.pl</Typography>
+    <Box sx={{
+      backgroundColor: "#D7D7D3", display: "flex",
+      justifyContent: "space-between", alignItems: "center",
+      py: 3, mt: "auto", flexDirection: { xs: "column", sm: "row" }
+    }}>
+      <Box sx={{ display: "flex", alignItems: "center", pl: 5 }}>
+        <img src={logo} alt="logo" width={50} height={50} />
+        <Typography sx={{ ml: 2 }} variant="h6">nowaposiadlosc.pl</Typography>
       </Box>
-      <Box>
-        <ul>
-          <li>Kontakt</li>
-          <li>O nas</li>
-          <li>Zgłoś problem</li>
+      <Box sx={{ pr: 5 }}>
+        <ul style={{ listStyle: "none", display: "flex" }}>
+          <li style={{ marginRight: "0.7rem" }}>
+            <Link href="/" color="black" underline="none">
+              {'Kontakt'}
+            </Link>
+          </li>
+          <li style={{ marginRight: "0.7rem" }}>
+            <Link href="/" color="black" underline="none">
+              {'O nas'}
+            </Link>
+          </li>
+          <li>
+            <Link href="/" color="black" underline="none">
+              {'Zgłoś problem'}
+            </Link>
+          </li>
         </ul>
       </Box>
     </Box>
